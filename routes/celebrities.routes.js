791ce64a742d880c589099af79a6cfe984651cf6
BodyElvis <bodymoliki@gmail.com>
2023-02-20 20:36:37 +0100

    iteration 0 - 9

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,60 @@
+/*
+const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+
+router.get("/celebrities", async (req, res, next) => {
+  try {
+    const foundCelebs = await Celebrity.find();
+    res.render("celebrities/celebrities.hbs", { celebsList: foundCelebs });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity.hbs");
+});
+
+router.post("/celebrities/create", async (req, res) => {
+  try {
+    const { name, occupation, catchPhrase } = req.body;
+    await Celebrity.create({ name, occupation, catchPhrase });
+    res.redirect("/celebrities");
+  } catch (error) {
+    console.log(error);
+    res.render("celebrities/new-celebrity.hbs");
+  }
+});
+
+module.exports = router;
+*/
+
+const express = require('express');
+const router = express.Router();
+const Celebrity = require("../models/Celebrity.model");
+
+router.get("/celebrities", async (req, res, next) => {
+  try {
+    const foundCelebs = await Celebrity.find();
+    res.render("celebrities/celebrities.hbs", { celebsList: foundCelebs });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity.hbs");
+});
+
+router.post("/celebrities/create", async (req, res) => {
+  try {
+    const { name, occupation, catchPhrase } = req.body;
+    await Celebrity.create({ name, occupation, catchPhrase });
+    res.redirect("/celebrities");
+  } catch (error) {
+    console.log(error);
+    res.render("celebrities/new-celebrity.hbs");
+  }
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,70 @@
+
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+
+// Route to display a list of all celebrities
+router.get("/celebrities", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("celebrities/celebrities", { celebrities });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// Route to display the form to create a new celebrity
+router.get("/celebrities/new", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
+
+
+// Route to handle the creation of a new celebrity
+router.post("/celebrities", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      res.render("celebrities/new-celebrity");
+    });
+});
+
+
+// Route to display a list of all movies
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      res.render("movies/movies", { movies });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// Route to display the form to create a new movie
+router.get("/movies/new", (req, res, next) => {
+  res.render("movies/new-movie");
+});
+
+// Route to handle the creation of a new movie
+router.post("/movies", (req, res, next) => {
+  const { title, genre, plot } = req.body;
+
+  Movie.create({ title, genre, plot })
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => {
+      res.render("movies/new-movie");
+    });
 });
 
 module.exports = router;
